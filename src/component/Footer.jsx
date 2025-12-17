776deba39b1a9fe5dec14ff6c2c9106b7c18a401
Techon-info <techoninfotech2024@gmail.com>
2025-12-17 12:31:24 +0530

    Update Footer component with new contact details loaction update

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -113,7 +113,7 @@ const Footer = () => {
                 <svg viewBox="0 0 24 24" fill="currentColor">
                   <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/>
                 </svg>
-                <span>Tiruchengode, Tamil Nadu, India</span>
+                <span>Coimbatore, Tamil Nadu, India</span>
               </li>
               <li>
                 <svg viewBox="0 0 24 24" fill="currentColor">
@@ -181,3 +181,4 @@ const Footer = () => {
 };
 
 export default Footer;
+
